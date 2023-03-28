897aca7fefb9491aa2201b16695248895ffb79db
Connor S <constroh@gmail.com>
2023-03-28 18:07:03 -0500

    questions array

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,47 @@ const highScorePage = document.querySelector("#highScorePage");
 const highScores = document.querySelector("#highScores");
 const mainBtn = document.querySelector("#main");
 const clearBtn = document.querySelector("#clear");
+
+// Questions array
+const questions = [
+  {
+    title: "What's the correct term?",
+    optionA: "Java",
+    optionB: "JavaScript",
+    optionC: "JavaJuice",
+    optionD: "JambaScript",
+    answer: "B",
+  },
+  {
+    title: "Commonly used data types don't include... ?",
+    optionA: "Booleans",
+    optionB: "Strings",
+    optionC: "Numbers",
+    optionD: "Square brackets",
+    answer: "D",
+  },
+  {
+    title: "Inside which HTML element do we put the JavaScript?",
+    optionA: "Script",
+    optionB: "Header",
+    optionC: "Span",
+    optionD: "Button",
+    answer: "A",
+  },
+  {
+    title: "JavaScript is a __________ language.",
+    optionA: "Spanish",
+    optionB: "Styling",
+    optionC: "Scripting",
+    optionD: "None of the above",
+    answer: "C",
+  },
+  {
+    title: "What symbol is used to compare if two values are stricly equal?",
+    optionA: "==",
+    optionB: "=",
+    optionC: "===",
+    optionD: "====",
+    answer: "C",
+  },
+];
